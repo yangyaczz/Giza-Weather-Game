c972ff48b3dcc8be340a6edbca9dbb4566defb80
yyczz <a729746954@gmail.com>
2024-06-03 00:01:52 +0800

    feat: script

--- create_prediction_round.py
+++ create_prediction_round.py
@@ -1,7 +1,7 @@
 import pprint
 import numpy as np
 from PIL import Image
-
+import requests
 from giza.agents import GizaAgent, AgentResult
 
 from ape import accounts
@@ -15,14 +15,33 @@ VERSION_ID = 4
 # As we are executing in sepolia, we need to specify the chain
 CHAIN = "ethereum:sepolia:geth"
 # The address of the deployed contract
-GWG_CONTRACT = "0x17807a00bE76716B91d5ba1232dd1647c4414912"  ###
+GWG_CONTRACT = "0x7EF2CFc86513ec79b8C8DE742a0991be2798A8e9"  ###
+
+URL = "https://api.weatherxm.com/api/v1/cells/8729a1d82ffffff/devices"
 
 
 def get_current_data_from_WeatherXM():
 
-    data = [12.5,95.0,0.19,1008.35,False]
+    # data = [12.5,95.0,0.19,1008.35,False]
+    response = requests.get(URL)
+    if response.status_code == 200:
+        data = response.json()
 
-    return np.array(data, dtype=object)
+        results = []
+        for device in data:
+            weather_data = device['current_weather']
+            result = {
+                'temperature': weather_data['temperature'],
+                'humidity': weather_data['humidity'],
+                'wind_speed': weather_data['wind_speed'],
+                'pressure': weather_data['pressure'],
+                'precipitation': bool(weather_data['precipitation'])
+            }
+            results.append(result)
+
+    print(results)
+
+    return np.array(results, dtype=object)
 
 
 def create_agent(model_id: int, version_id: int, chain: str, contract: str):
@@ -50,6 +69,12 @@ def predict(agent: GizaAgent, input):
 def get_probability(prediction: AgentResult):
     return int(prediction.value * 1000)
 
+def execute_contract(agent: GizaAgent, pob: int):
+
+    with agent.execute() as contracts:
+        contract_result = contracts.gwg.createRound(1717344058, pob)
+    return contract_result
+
 
 
 def create_prediction_round():
@@ -62,7 +87,11 @@ def create_prediction_round():
 
     probability = get_probability(prediction)
 
+    execute_contract(agent, probability)
+
     print('probability', probability)
+
+
 
 
 import os
